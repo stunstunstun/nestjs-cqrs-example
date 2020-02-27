723bb85e854eb7570ce152995ebeed43176f1cbc
minhyeok-jung <minhyeok.jung@navercorp.com>
2020-02-27 21:08:10 +0900

    :wrench: Add init script as development

---
+++ scripts/init_db.js
@@ -0,0 +1,4 @@
+db.getCollection('mileages').insertMany([
+  { userId: '3ede0ef2-92b7-4817-a5f3-0c575361f745', amount: 0, updated: new Date(), created: new Date() },
+  { userId: '3ede0ef2-92b7-4817-a5f3-0c575361f746', amount: 0, updated: new Date(), created: new Date() },
+]);
